f0d7a89bbff88492373f15506321f4402d24d024
Mayuge <mayuge.studio.public@gmail.com>
2023-11-02 19:39:56 +0900

    feat: Implement NewCommand Function

--- cli/command.go
+++ cli/command.go
@@ -1,5 +1,10 @@
 package cli
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Action func(args []string, opts []string) (string, error)
 type UsageFunc func() string
 
@@ -9,6 +14,26 @@ type Command struct {
 	Options   []string
 	Action    Action
 	Usage     UsageFunc
+}
+
+func NewCommand(name string, args []string, opts []string, action Action) *Command {
+	return &Command{
+		Name:      name,
+		Arguments: args,
+		Options:   opts,
+		Action:    action,
+		Usage: func() string {
+			var builder strings.Builder
+			builder.WriteString(fmt.Sprintf("Usage: %s", name))
+			if len(args) > 0 {
+				builder.WriteString(" [arguments]")
+			}
+			if len(args) > 0 {
+				builder.WriteString(" [options]")
+			}
+			return builder.String()
+		},
+	}
 }
 
 func (c *Command) Execute() (string, error) {
--- cli/command_test.go
+++ cli/command_test.go
@@ -24,20 +24,20 @@ func TestCommand_Execute(t *testing.T) {
 				option:   nil,
 			},
 			function: func(args []string, _ []string) (string, error) {
-				a, err := strconv.ParseInt(args[0], 10, 64)
+				a, err := strconv.Atoi(args[0])
 				if err != nil {
 					return "", err
 				}
-				b, err := strconv.ParseInt(args[1], 10, 64)
+				b, err := strconv.Atoi(args[1])
 				if err != nil {
 					return "", err
 				}
-				return fmt.Sprintf("%d", a+b), nil
+				return strconv.Itoa(a + b), nil
 			},
 			want: "4",
 		},
 		{
-			name: "1+1",
+			name: "Hello+World",
 			args: args{
 				argument: []string{"Hello", "World"},
 				option:   nil,
@@ -64,6 +64,54 @@ func TestCommand_Execute(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewCommand(t *testing.T) {
+	tests := []struct {
+		testName string
+		args     []string
+		opts     []string
+		wantName string
+	}{
+		{
+			testName: "simple command",
+			args:     []string{"arg1", "arg2"},
+			opts:     []string{"--opt1", "--opt2"},
+			wantName: "test-command",
+		},
+		{
+			testName: "simple with no args and opts",
+			args:     []string{},
+			opts:     []string{},
+			wantName: "simple-command",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			cmd := NewCommand(tt.wantName, tt.args, tt.opts, nil)
+
+			if cmd.Name != tt.wantName {
+				t.Errorf("NewCommand() name = %v, want %v", cmd.Name, tt.wantName)
+			}
+			if len(cmd.Arguments) != len(tt.args) {
+				t.Errorf("NewCommand() Argument length = %v, want %v", len(cmd.Arguments), len(tt.args))
+			}
+			for i, arg := range cmd.Arguments {
+				if arg != tt.args[i] {
+					t.Errorf("NewCommand() Argument[%d] = %v, want %v", i, arg, tt.args[i])
+				}
+			}
+			if len(cmd.Options) != len(tt.opts) {
+				t.Errorf("NewCommand() Options length = %v, want %v", len(cmd.Options), len(tt.opts))
+			}
+			for i, opt := range cmd.Options {
+				if opt != tt.opts[i] {
+					t.Errorf("NewCommand() Options[%d] = %v, want %v", i, opt, tt.opts[i])
+				}
 			}
 		})
 	}
