52efa0a5a0a0ebd0564bd58b8d6db5a84beed6cf
Mayuge <mayuge.studio.public@gmail.com>
2023-11-02 10:02:12 +0900

    feat: Implement Command struct

--- cli/cli.go
+++ cli/cli.go
@@ -1,1 +1,9 @@
 package cli
+
+type Func func(args []string, opts []string) (string, error)
+
+type Command struct {
+	arguments []string
+	options   []string
+	function  Func
+}
---
+++ cli/cli_test.go
@@ -0,0 +1,70 @@
+package cli
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func TestCommand(t *testing.T) {
+	type args struct {
+		argument []string
+		option   []string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		function Func
+		want     string
+	}{
+		{
+			name: "3+1",
+			args: args{
+				argument: []string{"3", "1"},
+				option:   nil,
+			},
+			function: func(args []string, _ []string) (string, error) {
+				a, err := strconv.ParseInt(args[0], 10, 64)
+				if err != nil {
+					return "", err
+				}
+				b, err := strconv.ParseInt(args[1], 10, 64)
+				if err != nil {
+					return "", err
+				}
+				return fmt.Sprintf("%d", a+b), nil
+			},
+			want: "4",
+		},
+		{
+			name: "1+1",
+			args: args{
+				argument: []string{"Hello", "World"},
+				option:   nil,
+			},
+			function: func(args []string, _ []string) (string, error) {
+				a := args[0]
+				b := args[1]
+				return fmt.Sprintf("%s %s", a, b), nil
+			},
+			want: "Hello World",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := &Command{
+				arguments: tt.args.argument,
+				options:   tt.args.option,
+				function:  tt.function,
+			}
+			got, err := cmd.function(tt.args.argument, tt.args.option)
+			if err != nil {
+				t.Errorf("got error %v", err)
+			}
+			if got != tt.want {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
