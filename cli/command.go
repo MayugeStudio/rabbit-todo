a41bb5ceff4871695630cb0f329ea13a803eeb7d
Mayuge <mayuge.studio.public@gmail.com>
2023-11-02 18:07:53 +0900

    add: add Usage attribute to Command Struct

--- cli/command.go
+++ cli/command.go
@@ -1,12 +1,14 @@
 package cli
 
 type Action func(args []string, opts []string) (string, error)
+type UsageFunc func() string
 
 type Command struct {
 	Name      string
 	Arguments []string
 	Options   []string
 	Function  Action
+	Usage     UsageFunc
 }
 
 func (c *Command) Execute() (string, error) {
