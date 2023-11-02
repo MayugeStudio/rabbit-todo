0ed5be50750ac174139c0bb082e0daf442b66773
Mayuge <mayuge.studio.public@gmail.com>
2023-11-03 04:35:16 +0900

    refactor: Refactor test code

--- cli/command_test.go
+++ cli/command_test.go
@@ -4,6 +4,101 @@ import (
 	"testing"
 )
 
+func TestNewCommand(t *testing.T) {
+	tests := []struct {
+		testName    string
+		commandName string
+		args        []string
+		opts        []string
+	}{
+		{
+			testName:    "With two arguments and two options",
+			commandName: "test-command",
+			args:        []string{"arg1", "arg2"},
+			opts:        []string{"--opt1", "--opt2"},
+		},
+		{
+			testName:    "With no arguments and options",
+			commandName: "test-command",
+			args:        []string{},
+			opts:        []string{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			cmd := NewCommand(tt.commandName, tt.args, tt.opts, nil)
+
+			if cmd.Name != tt.commandName {
+				t.Errorf("got: name = %v, want %v", cmd.Name, tt.commandName)
+			}
+			if len(cmd.Arguments) != len(tt.args) {
+				t.Errorf("got: Argument length = %v, want %v", len(cmd.Arguments), len(tt.args))
+			}
+			for i, arg := range cmd.Arguments {
+				if arg != tt.args[i] {
+					t.Errorf("got: Argument[%d] = %v, want %v", i, arg, tt.args[i])
+				}
+			}
+			if len(cmd.Options) != len(tt.opts) {
+				t.Errorf("got: Options length = %v, want %v", len(cmd.Options), len(tt.opts))
+			}
+			for i, opt := range cmd.Options {
+				if opt != tt.opts[i] {
+					t.Errorf("got: Options[%d] = %v, want %v", i, opt, tt.opts[i])
+				}
+			}
+		})
+	}
+}
+
+func TestCommand_Usage(t *testing.T) {
+	tests := []struct {
+		testName    string
+		commandName string
+		args        []string
+		opts        []string
+		want        string
+	}{
+		{
+			testName:    "With 1 arg and 1 opt",
+			commandName: "test-command",
+			args:        []string{"Hello"},
+			opts:        []string{"--hello"},
+			want:        "Usage: test-command [arguments] [options]",
+		},
+		{
+			testName:    "With 2 arg and 2 opt",
+			commandName: "test-command",
+			args:        []string{"Hello", "World"},
+			opts:        []string{"--hello", "--world"},
+			want:        "Usage: test-command [arguments] [options]",
+		},
+		{
+			testName:    "With 1 arg and 0 opt",
+			commandName: "test-command",
+			args:        []string{"OneArg"},
+			opts:        []string{},
+			want:        "Usage: test-command [arguments]",
+		},
+		{
+			testName:    "With 0 arg and 1 opt",
+			commandName: "test-command",
+			args:        []string{},
+			opts:        []string{"OneOpt"},
+			want:        "Usage: test-command [options]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			cmd := NewCommand(tt.commandName, tt.args, tt.opts, nil)
+			if cmd.Usage != tt.want {
+				t.Errorf("got: %v, want: %v", cmd.Usage, tt.want)
+			}
+		})
+	}
+}
+
 func TestCommand_Execute(t *testing.T) {
 	var testFunction Action
 	testFunction = func(args []string, opts []string) (string, error) {
@@ -48,102 +143,7 @@ func TestCommand_Execute(t *testing.T) {
 				t.Errorf("got: error %v", err)
 			}
 			if got != tt.want {
-				t.Errorf("got: %v, length: %d, want: %v, length: %d", got, len(got), tt.want, len(tt.want))
-			}
-		})
-	}
-}
-
-func TestNewCommand(t *testing.T) {
-	tests := []struct {
-		testName string
-		args     []string
-		opts     []string
-		wantName string
-	}{
-		{
-			testName: "simple command",
-			args:     []string{"arg1", "arg2"},
-			opts:     []string{"--opt1", "--opt2"},
-			wantName: "test-command",
-		},
-		{
-			testName: "simple with no args and opts",
-			args:     []string{},
-			opts:     []string{},
-			wantName: "simple-command",
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.testName, func(t *testing.T) {
-			cmd := NewCommand(tt.wantName, tt.args, tt.opts, nil)
-
-			if cmd.Name != tt.wantName {
-				t.Errorf("NewCommand() name = %v, want %v", cmd.Name, tt.wantName)
-			}
-			if len(cmd.Arguments) != len(tt.args) {
-				t.Errorf("NewCommand() Argument length = %v, want %v", len(cmd.Arguments), len(tt.args))
-			}
-			for i, arg := range cmd.Arguments {
-				if arg != tt.args[i] {
-					t.Errorf("NewCommand() Argument[%d] = %v, want %v", i, arg, tt.args[i])
-				}
-			}
-			if len(cmd.Options) != len(tt.opts) {
-				t.Errorf("NewCommand() Options length = %v, want %v", len(cmd.Options), len(tt.opts))
-			}
-			for i, opt := range cmd.Options {
-				if opt != tt.opts[i] {
-					t.Errorf("NewCommand() Options[%d] = %v, want %v", i, opt, tt.opts[i])
-				}
-			}
-		})
-	}
-}
-
-func TestCommand_Usage(t *testing.T) {
-	tests := []struct {
-		testName    string
-		commandName string
-		args        []string
-		opts        []string
-		want        string
-	}{
-		{
-			testName:    "test with 1 arg and 1 opt",
-			commandName: "test-command",
-			args:        []string{"Hello"},
-			opts:        []string{"--hello"},
-			want:        "Usage: test-command [arguments] [options]",
-		},
-		{
-			testName:    "test with 2 arg and 2 opt",
-			commandName: "test-command",
-			args:        []string{"Hello", "World"},
-			opts:        []string{"--hello", "--world"},
-			want:        "Usage: test-command [arguments] [options]",
-		},
-		{
-			testName:    "test with 1 arg and 0 opt",
-			commandName: "test-command",
-			args:        []string{"OneArg"},
-			opts:        []string{},
-			want:        "Usage: test-command [arguments]",
-		},
-		{
-			testName:    "test with 0 arg and 1 opt",
-			commandName: "test-command",
-			args:        []string{},
-			opts:        []string{"OneOpt"},
-			want:        "Usage: test-command [options]",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.testName, func(t *testing.T) {
-			cmd := NewCommand(tt.commandName, tt.args, tt.opts, nil)
-			if cmd.Usage != tt.want {
-				t.Errorf("got %v, want %v", cmd.Usage, tt.want)
+				t.Errorf("got: %v, want: %v", got, tt.want)
 			}
 		})
 	}
