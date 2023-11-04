1994235d4660affc0f1d3fa5c1b26dc51e7b0842
Mayuge <mayuge.studio.public@gmail.com>
2023-11-05 01:31:53 +0900

    commend: Write comment for command.go

--- cli/command.go
+++ cli/command.go
@@ -10,8 +10,10 @@ const (
 	optionPrefix = "--"
 )
 
-type Action func(args []string, opts map[string]param.Value) (string, error)
-
+// Command represents a CLI command, including its name, expected arguments,
+// options, the action to execute, and usage information.
+// The struct is used to define commands in a CLI application and provides
+// methods to execute and validate command input.
 type Command struct {
 	Name      string
 	Arguments []*param.Argument
@@ -20,7 +22,16 @@ type Command struct {
 	Usage     string
 }
 
-// Execute method execute action
+// Action defines the function signature for actions that commands execute.
+// It takes a slice of arguments and a map of options, which are parsed from the CLI input.
+// The function returns a string that usually contains the result of the command execution or
+// a message to the user, and an error if the execution fails.
+type Action func(args []string, opts map[string]param.Value) (string, error)
+
+// Execute runs the command with the provided input parameters.
+// It separates the input parameters into arguments and options,
+// validates them, and then calls the commands' Action function.
+// It returns the result-string of the Action function or an error encountered during validation or execution.
 func (c *Command) Execute(inputParams []string) (string, error) {
 	args, opts, err := c.Validate(inputParams)
 	if err != nil {
@@ -30,6 +41,12 @@ func (c *Command) Execute(inputParams []string) (string, error) {
 	return c.Action(args, opts)
 }
 
+// Validate parses and validates the input parameters for the command.
+// It separates the input parameters into arguments and options,
+// checks them against the command's requirements, and returns
+// a slice of arguments and a map of option if they are valid.
+// It returns an error if there are too few or too many arguments,
+// or if an invalid option is provided.
 func (c *Command) Validate(inputParams []string) ([]string, map[string]param.Value, error) {
 	args := make([]string, 0, len(c.Arguments))
 	opts := c.initializeOptions()
@@ -56,6 +73,10 @@ func (c *Command) Validate(inputParams []string) ([]string, map[string]param.Val
 	return args, opts, nil
 }
 
+// parseOption takes an option parameter and the current index of inputParams,
+// determines whether it's a flag or a regular option, and then processes it accordingly.
+// It returns the name of the option, its value, and an error if the option is invalid
+// or if there's a problem processing the value.
 func (c *Command) parseOption(optParam string, inputParams []string, idxPtr *int, flagOpts []*param.Option) (string, *param.Value, error) {
 	optType, isFlag, err := c.getOptionTypeAndFlag(optParam)
 	if err != nil {
@@ -69,7 +90,8 @@ func (c *Command) parseOption(optParam string, inputParams []string, idxPtr *int
 	}
 }
 
-// validateArguments ensures the correct number of arguments have been provided.
+// validateArguments checks if the correct number of the arguments has been provided for the command.
+// It returns an error if the number of provided arguments is less than required or greater than allowed.
 func (c *Command) validateArguments(args []string) error {
 	if len(args) < len(c.Arguments) {
 		return fmt.Errorf("not enough arguments: actual %d, expected %d", len(args), len(c.Arguments))
@@ -79,7 +101,9 @@ func (c *Command) validateArguments(args []string) error {
 	return nil
 }
 
-// initializeOptions initializes the options map with default values for flags.
+// initializeOptions creates a map with default values for all options defined in the command.
+// It initializes flags with a default boolean value of false.
+// The map is used to store the values of options parses from the input parameters.
 func (c *Command) initializeOptions() map[string]param.Value {
 	options := make(map[string]param.Value)
 	for _, option := range c.Options {
@@ -90,6 +114,9 @@ func (c *Command) initializeOptions() map[string]param.Value {
 	return options
 }
 
+// getOptionTypeAndFlag  retrieves the type and flag status of the option with the given name.
+// It returns the type of the option, a boolean indicating if it's a flag,
+// if the option does not exist in the command's options list.
 func (c *Command) getOptionTypeAndFlag(optionName string) (param.Type, bool, error) {
 	for _, option := range c.Options {
 		if option.Name == optionName {
@@ -99,6 +126,10 @@ func (c *Command) getOptionTypeAndFlag(optionName string) (param.Type, bool, err
 	return -1, false, fmt.Errorf("invalid option %s", optionName)
 }
 
+// processFlagOption processes a flag option from the input parameters.
+// A flag option does not take a value; it is simply present or absent.
+// The method updates the flagOpts slice to remove processed options and
+// returns the name of the option and its boolean value.
 func (c *Command) processFlagOption(optionName string, inputParams []string, idxPtr *int, flagOpts []*param.Option) (string, *param.Value, error) {
 	// Make sure the next parameter is not an argument not starting with `--`
 	// Flag Option cannot have value
@@ -112,6 +143,9 @@ func (c *Command) processFlagOption(optionName string, inputParams []string, idx
 	return optionName, param.NewBoolParameterPtr(true), nil
 }
 
+// processRegularOption processes a regular (non-flag) option.
+// It checks if the next parameter is a valid value for the option.
+// parses it, and returns the option's name and its parsed value.
 func (c *Command) processRegularOption(optionName string, optType param.Type, inputParams []string, idxPtr *int) (string, *param.Value, error) {
 	// Make sure the  next parameter is not an option starting with `--`
 	// Whether normal option is last parameter or not
@@ -133,6 +167,8 @@ func (c *Command) processRegularOption(optionName string, optType param.Type, in
 	return optionName, paramValue, nil
 }
 
+// removeFlagOption removes a flag option from the slice of flag options once it has been processed.
+// This ensures that each flag  option is only processed once.
 func (c *Command) removeFlagOption(name string, flagOpts []*param.Option) {
 	for i, opt := range flagOpts {
 		if opt.Name == name {
@@ -143,6 +179,8 @@ func (c *Command) removeFlagOption(name string, flagOpts []*param.Option) {
 	}
 }
 
+// NewCommand constructs a new Command object with the given name, arguments, options, and action.
+// It also generates a usage string that explains how to call the command.
 func NewCommand(name string, args []*param.Argument, opts []*param.Option, action Action) Command {
 	return Command{
 		Name:      name,
@@ -153,6 +191,8 @@ func NewCommand(name string, args []*param.Argument, opts []*param.Option, actio
 	}
 }
 
+// isArgument checks if the provided string is an argument, i.e., does not start with '--'.
+// It returns true if the string is an argument, false otherwise.
 func isArgument(p string) bool {
 	if strings.HasPrefix(p, "--") {
 		return false
@@ -160,6 +200,8 @@ func isArgument(p string) bool {
 	return true
 }
 
+// flagOptions constructs a lice of all flag options from the command's options.
+// Flag options are options that do not take an argument and act as boolean toggles.
 func (c *Command) flagOptions() []*param.Option {
 	var flagOpts []*param.Option
 
@@ -171,6 +213,9 @@ func (c *Command) flagOptions() []*param.Option {
 	return flagOpts
 }
 
+// createUsageString generates a usage string for the command that includes its name,
+// a placeholder for arguments, and a placeholder for options if the command has any.
+// The generated string is intended to be shown to users to demonstrate how to use the command.
 func createUsageString(commandName string, args []*param.Argument, opts []*param.Option) string {
 	var builder strings.Builder
 	builder.WriteString(fmt.Sprintf("Usage: %s", commandName))
