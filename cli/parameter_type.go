06e2062a502968be9519d3f94c0fc57d94f6d858
Mayuge <mayuge.studio.public@gmail.com>
2023-11-04 21:45:08 +0900

    test: Write test for parameter_type.go

--- cli/parameter_type.go
+++ cli/parameter_type.go
@@ -7,3 +7,16 @@ const (
 	INT
 	BOOL
 )
+
+func ParameterTypeToString(paramType ParameterType) string {
+	switch paramType {
+	case BOOL:
+		return "bool"
+	case INT:
+		return "int"
+	case STRING:
+		return "string"
+	default:
+		return "unknownType"
+	}
+}
